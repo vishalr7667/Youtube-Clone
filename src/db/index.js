c41fe5467d7c6c785ee3996c1971cb2eb4c8f36d
Vishal Kumar <76913781+vishalr7667@users.noreply.github.com>
2025-01-16 15:39:17 +0530

    changes in db file

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,6 @@ import {DB_NAME} from "../constants.js";
 const connectDb = async () => {
 	try {
 	 const connectionInstance =	await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-	 console.log(`\n MongoDb Connected !! DB HOST ${connectionInstance}`);
 	 console.log(`\n MongoDb Connected !! DB HOST ${connectionInstance.connection.host}`);
 	 
 	} catch (error) {
