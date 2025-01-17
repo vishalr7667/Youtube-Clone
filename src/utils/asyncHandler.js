24f48d0b84dba3183dd7e7964d2f0a6da87dd256
Vishal Kumar <76913781+vishalr7667@users.noreply.github.com>
2025-01-17 10:26:40 +0530

    changes in utils code

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,16 @@
+// const asyncHandler = (requestHandler) => {
+// 	return (req, res, next) => {
+// 		Promise.resolve(requestHandler()).catch((err)=> next(err))
+// 	}
+// }
+
 const asyncHandler = (requestHandler) => {
-	(req, res, next) => {
+	return (req, res, next) => {
 		Promise.resolve(requestHandler()).catch((err)=> next(err))
 	}
 }
+
+// The asyncHandler takes a requestHandler function (typically a route handler) as a parameter and returns a middleware function. Inside this middleware, the requestHandler is executed within a Promise.resolve. This ensures that both synchronous and asynchronous code is treated uniformly. If any error occurs within the requestHandler (e.g., via throw or an unhandled rejection), it is caught in the .catch block and passed to next() as an error. Express then automatically invokes the centralized error-handling middleware to handle this error. This design ensures cleaner routes and centralized error management.
 
 export {asyncHandler}
 
